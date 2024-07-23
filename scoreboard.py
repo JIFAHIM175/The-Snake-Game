9e3503924c56aec01e610bfeb72be1ac4746d5e8
jiFahim175 <jf9858q@gre.ac.uk>
2024-07-23 23:27:07 +0100

    The second commit

--- scoreboard.py
+++ scoreboard.py
@@ -5,6 +5,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,3 +23,7 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
